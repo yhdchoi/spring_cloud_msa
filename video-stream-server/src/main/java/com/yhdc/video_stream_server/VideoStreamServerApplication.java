50417fb66e29d614113d4845af8daabcb2bbf24f
yhdchoi <yhdchoi@gmail.com>
2025-04-04 20:36:53 +0900

    .gitignore refactored Video Stream server loaded

---
+++ video-stream-server/src/main/java/com/yhdc/video_stream_server/VideoStreamServerApplication.java
@@ -0,0 +1,13 @@
+package com.yhdc.video_stream_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStreamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStreamServerApplication.class, args);
+	}
+
+}
---
+++ video-stream-server/src/test/java/com/yhdc/video_stream_server/VideoStreamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhdc.video_stream_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
