e3d5742ab85a0475206d4b96013803ab2e8aa71f
yhdchoi <yhdchoi@gmail.com>
2025-04-26 20:44:25 +0900

    Refactor - rename

--- store-service/src/test/java/com/yhdc/store_service/TestStoreServiceApplication.java
+++ store-service/src/test/java/com/yhdc/store_service/TestStoreServiceApplication.java
@@ -2,7 +2,7 @@ package com.yhdc.store_service;
 
 import org.springframework.boot.SpringApplication;
 
-public class TestStoreServerApplication {
+public class TestStoreServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.from(StoreServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
