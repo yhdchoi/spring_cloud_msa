1c530f6e60691d29fb61e168a8773f064d032349
yhdchoi <yhdchoi@gmail.com>
2025-04-26 20:23:01 +0900

    Refactor - rename

--- account-service/src/main/java/com/yhdc/account_service/AccountServiceApplication.java
+++ account-service/src/main/java/com/yhdc/account_service/AccountServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AccountServerApplication {
+public class AccountServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AccountServerApplication.class, args);
+        SpringApplication.run(AccountServiceApplication.class, args);
     }
 
 }
